f72ba9e561070b6771371c8da4fb78bceea2165a
Lauren Voswinkel <lvoswinkel@hashicorp.com>
2020-10-22 15:22:05 -0700

    Remove Run() as it is only used in standalone plugin setup (#18)
    * Remove Run() as it is only used in standalone plugin setup

--- mongodbatlas.go
+++ mongodbatlas.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/hashicorp/vault/api"
 	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
 	"github.com/hashicorp/vault/sdk/database/helper/credsutil"
 	"github.com/hashicorp/vault/sdk/database/helper/dbutil"
@@ -37,18 +36,6 @@ func new() *MongoDBAtlas {
 	return &MongoDBAtlas{
 		mongoDBAtlasConnectionProducer: connProducer,
 	}
-}
-
-// Run instantiates a MongoDBAtlas object, and runs the RPC server for the plugin
-func Run(apiTLSConfig *api.TLSConfig) error {
-	dbType, err := New()
-	if err != nil {
-		return err
-	}
-
-	dbplugin.Serve(dbType.(dbplugin.Database), api.VaultPluginTLSProvider(apiTLSConfig))
-
-	return nil
 }
 
 func (m *MongoDBAtlas) Initialize(ctx context.Context, req dbplugin.InitializeRequest) (dbplugin.InitializeResponse, error) {
